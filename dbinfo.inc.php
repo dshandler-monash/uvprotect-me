09e0ef437a6f402dd64e0dd17e2e24781285d946
Darren Shandler <dsha0047@student.monash.edu>
2022-08-13 20:33:34 +1000

    change .inc file

--- dbinfo.inc.php
+++ dbinfo.inc.php
@@ -6,7 +6,7 @@ $db_password = 'uvprotectme_123';
 $db_database = 'location';
 
 /* Connect to MySQL and select the database. */
-$conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
+$conn = mysqli_connect($db_server, $db_username, $db_password, 'Connect');
 
 
 ?>
