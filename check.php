c20c5cb5b946c228d9bb2a15a7ab7ef69404304b
Darren Shandler <dsha0047@student.monash.edu>
2022-08-13 21:46:16 +1000

    change echo

--- check.php
+++ check.php
@@ -110,7 +110,7 @@ $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database, 330
 												$sql_query = "SELECT * FROM suburb LIMIT 10";
 												$result = mysqli_query($conn, $sql_query);
 												while($query_data = mysqli_fetch_row($result)) {
-													echo $query_data[0];
+													echo $query_data[2];
 												}
 											?>
 										</table>
