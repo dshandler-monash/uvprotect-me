32773c1b040f7c5d023ef01978cfed0d2e4d5e38
Darren Shandler <dsha0047@student.monash.edu>
2022-08-13 21:04:47 +1000

    test without php

--- check.php
+++ check.php
@@ -97,20 +97,7 @@
 												<th>Column 5</th>
 												<th>Column 6</th>
 											</tr>
-											<?php
-												$database = mysqli_select_db($conn, $db_database);
-												$result = mysqli_query($database, "SELECT * FROM suburb LIMIT 10");
-												while($query_data = mysqli_fetch_row($result)) {
-													echo "<tr>
-															<td>"$query_data[0]"</td>
-															<td>"$query_data[1]"</td>
-															<td>"$query_data[2]"</td>
-															<td>"$query_data[3]"</td>
-															<td>"$query_data[4]"</td>
-															<td>"$query_data[5]"</td>
-														</tr>";
-												}
-											?>
+											
 										</table>
 									</section>
 									
