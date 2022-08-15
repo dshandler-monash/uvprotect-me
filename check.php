af8c0433439d56dc4598adeda2bcc95aa6ac6b94
Darren Shandler <dsha0047@student.monash.edu>
2022-08-15 20:49:50 +1000

    add htaccess file

--- check.php
+++ check.php
@@ -15,9 +15,6 @@ $db_database = 'location';
 $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database, 3306);
 ?>
 
-<filesMatch ".(ico|pdf|flv|jpg|jpeg|png|gif|js|css|swf)$">
-    Header set Cache-Control "max-age=60, public"
-</filesMatch>
 
 <html>
 	<head>
